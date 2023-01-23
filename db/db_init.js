56607bcd4d8d91eff98bc2fbf79718fedf89fdf0
rabidsalid <alishahmeer300@gmail.com>
2023-01-23 13:20:13 -0500

    commit

--- db/db_init.js
+++ db/db_init.js
@@ -1,15 +1,28 @@
 const db = require("./db_connection");
-const drop_stuff_table_sql = "DROP TABLE IF EXISTS `stuff`;"
+const fs = require("fs");
+
+const drop_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/drop_stuff_table.sql", { encoding: "UTF-8" })
+const create_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/create_stuff_table.sql", { encoding: "UTF-8" })
+const insert_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/insert_stuff_table.sql", { encoding: "UTF-8" })
+const read_stuff_table_sql = fs.readFileSync(__dirname + "/db/queries/init/read_stuff_table.sql", { encoding: "UTF-8" })
 
 db.execute(drop_stuff_table_sql);
-
-const create_stuff_table_sql = `
-    CREATE TABLE stuff (
-        id INT NOT NULL AUTO_INCREMENT,
-        item VARCHAR(45) NOT NULL,
-        quantity INT NOT NULL,
-        description VARCHAR(150) NULL,
-        PRIMARY KEY (id)
-    );
-`
 db.execute(create_stuff_table_sql);
+
+db.execute(insert_stuff_table_sql, ['Widgets', '5', 'Widgets are cool! You can do ... so many... different things... with them...']);
+db.execute(insert_stuff_table_sql, ['Gizmos', '100', null]);
+db.execute(insert_stuff_table_sql, ['Thingamajig', '12345', 'Not to be confused with a Thingamabob']);
+db.execute(insert_stuff_table_sql, ['Thingamabob', '54321', 'Not to be confused with a Thingamajig']);
+
+
+db.execute(read_stuff_table_sql, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'stuff' initialized with:")
+        console.log(results);
+    }
+);
+
+db.end();
