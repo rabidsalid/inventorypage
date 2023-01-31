bbd407ee8ed20b6a9feef287d15b6724df780425
rabidsalid <alishahmeer300@gmail.com>
2023-01-31 15:41:00 -0500

    commit

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require( "express" );
 const logger = require("morgan");
 const app = express();
 const port = 8080;
+const db = require('./db/db_connection');
+// configure express to use ejs
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 
@@ -10,17 +14,39 @@ app.use(express.static(__dirname + '/public'));
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
+    res.render("index");
 } );
+
+const read_stuff_all_sql = 'SELECT id, item, quantity, description FROM stuff'
 
 // define a route for the stuff inventory page
 app.get( "/invpage", ( req, res ) => {
-    res.sendFile( __dirname + "/views/invpage.html" );
+    db.execute(read_stuff_all_sql, (error, results) => {
+        if (error) {
+            res.status(500).send(error);
+        } else {
+            res.render("invpage", {inventory: results});
+        }
+    });
+    // res.sendFile( __dirname + "/views/invpage.html" );
 } );
 
+const read_item_sql = 'SELECT item, quantity, description FROM stuff WHERE id = ?'
+
 // define a route for the item detail page
-app.get( "/invpage/infopage", ( req, res ) => {
-    res.sendFile( __dirname + "/views/infopage.html" );
+app.get( "/invpage/infopage/:id", ( req, res ) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error) {
+            res.status(500).send(error);
+        } 
+        else if (results.length == 0) {
+            res.status(404).send(`No item found with id = ${req.params.id}`);
+        }
+        else {
+            res.render('infopage', results[0]);
+        }
+    });
+    // res.sendFile( __dirname + "/views/infopage.html" );
 } );
 
 // start the server
